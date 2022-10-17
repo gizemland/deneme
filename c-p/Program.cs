cdf0de6cb8cb50d7a37d4da7d2390bb4f471e547
gizemland <gizemsosyalm@gmail.com>
2022-10-17 15:46:46 +0300

    pratikler klasörü ilk commit

---
+++ c-p/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c_p
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("adınız?");
+            string name= Console.ReadLine();
+            Console.WriteLine("soyadınız?");
+            string surname=Console.ReadLine();
+            Console.WriteLine("merhaba"+" "+name+" "+surname);
+            
+        }
+    }
+}
